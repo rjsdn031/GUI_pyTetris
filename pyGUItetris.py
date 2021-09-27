6968a9f6ce4a5394de0a37ae2829afb9bd4e4ccd
rjsdn031 <rjsdn031@naver.com>
2021-09-28 02:40:39 +0900

    Edit some functions and comments

--- pyGUItetris.py
+++ pyGUItetris.py
@@ -3,36 +3,44 @@
 
 
 # board == playboard
-# stack == block stack
 
-from tkinter import * ## tkinter 기반 gui 구현.
+from tkinter import *
 import random as ran
-import time
 
 board = []
 blocks = [
 
-        [[1,1,1],
+        [[0,0,0],
+         [1,1,1],
          [0,1,0]],
 
         [[0,1,1],
-         [1,1,0]],
+         [1,1,0],
+         [0,0,0]],
 
         [[1,1,0],
-         [0,1,1]],
+         [0,1,1],
+         [0,0,0]],
 
         [[0,0,1],
-         [1,1,1]],
+         [1,1,1],
+         [0,0,0]],
 
-        [[1,1,1,1]],
+        [[1,0,0],
+         [1,1,1],
+         [0,0,0]],
+
+        [[0,1,0,0],
+         [0,1,0,0],
+         [0,1,0,0],
+         [0,1,0,0]],
 
         [[1,1],
          [1,1]],
-
 ]
 
-stack = []
 curBlock = []
+x,y = 0,0
 
 def init_board():
     global board
@@ -51,32 +59,22 @@ def choice_block():
     i = ran.randrange(6)
     return blocks[i]
 
-def init_stack():
-    global stack
-    for n in range(3):  ##stack size == 3
-        stack.append(choice_block())
-    return
-
-##편의상 작성한 코드
-def print_stack():
-    for i in stack:
-        print(i, end=" ")
-    return
-
 def set_curBlock():
-    global stack
     global curBlock
-    curBlock = stack[0]
-    for n in range(1,3):
-        stack[n-1] = stack[n]
-    
-    stack[2] = choice_block()
+    curBlock = choice_block()
     return
 
-def stackIsFull():  ##stack이 차있는가?
+def set_startpoint():   ##startpoint 설정. startpoint의 기준점은 block[0][0].
+    global x,y
+    x = int(len(board[0])/2)-int(len(curBlock[0])/2)
+    y = 0
+    return
+
+def draw_block():       ## 리스트 값을 없애고 리스트 안에 집어넣는 형식
     return
 
 def create_block():
+    set_startpoint()
     return
 
 def turn_block():
@@ -90,15 +88,9 @@ def play():
 ##main
 if __name__ == "__main__":
     init_board()
-    init_stack()
     print_board()
     print("\n")
-    print_stack()
     set_curBlock()
     print("\n")
     print(curBlock)
-    print_stack()
     
-
-    # how to make timer?
-    # 일단 구현하고 나중에 객체지향 시스템으로 바꾼다.
